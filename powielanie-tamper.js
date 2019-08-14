aa789dd13e950a1b80b6954abbfe066e3e94549d
Emilian Demus <emilian@demus.pl>
2019-08-14 12:39:09 +0200

    statusy

--- powielanie-tamper.js
+++ powielanie-tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Powiel we wszystkich jezykach
 // @namespace    demus.pl
-// @version      0.1
+// @version      0.2
 // @description  Powiel teksty w tabach jezykow
 // @author       You
 // @match        https://www.demus-zegarki.pl/panel/product-edit.php?idt=*
@@ -17,28 +17,76 @@ function tamperCopyToOther() {
     var productName = "input[id*='[productname]']";
     var productDescriptionShort = "textarea[id*='[description]']";
     var productDescriptionLong = "input[id*='tableRowTextEditTabs_container_']";
+    var $panes = $tabContent.find('.tab-pane');
+    var panesCount = $panes.length;
+
+    // setup status fields
+    var tamperStatusBox = '#tamperStatusBox';
+    var tamperProductName = '#tamperProductcName';
+    var tamperProductShortD = '#tamperProductcShortD';
+    var tamperProductLongD = '#tamperProductcLongD';
+    var tamperNumberOfTabs = '#tamperNumberOfTabs';
 
     // active tab values
     var nazwa = $tabContent.find('.tab-pane.active').find(productName).val();
     var opisK = $tabContent.find('.tab-pane.active').find(productDescriptionShort).val();
     var opisD = $tabContent.find('.tab-pane.active').find(productDescriptionLong).val();
 
+    // update statusBox
+    $(tamperProductName).text(nazwa);
+    $(tamperProductShortD).text(opisK);
+    $(tamperProductLongD).text(opisD);
+    if (opisD.length > 80) {
+        $(tamperProductLongD).text(opisD.substring(0, 80) + ' [...]');
+    }
+    $(tamperNumberOfTabs).text('0/' + panesCount);
+    $(tamperStatusBox).show();
+
+
     // clone values into all tab-panes
-    $tabContent.find('.tab-pane').each(function(i, item){
+    var tamperUpdatedTabs = 0;
+    $panes.each(function(i, item){
         var $item = $(item);
-        $item.find(productName).val(nazwa);
-        $item.find(productDescriptionShort).val(opisK);
-        $item.find(productDescriptionLong).val(opisD);
+        var success = true;
+        var $elName = $item.find(productName);
+        var $elDescShort = $item.find(productDescriptionShort);
+        var $elDescLong = $item.find(productDescriptionLong);
+
+        if ($elName.length && $elDescShort.length && $elDescLong.length) {
+            $elName.val(nazwa);
+            $elDescShort.val(opisK);
+            $elDescLong.val(opisD);
+            tamperUpdatedTabs += 1;
+        }
     });
+
+    // updated number of tabs
+    $(tamperNumberOfTabs).text(tamperUpdatedTabs + '/' + panesCount);
+    if (panesCount != tamperUpdatedTabs) {
+        $(tamperNumberOfTabs).text('!!!!!' + tamperUpdatedTabs + '/' + panesCount + '!!!!!');
+    }
 }
 
 
 var button = '<li><a href="#" id="tamperCopyToOther" style="background-color: yellowgreen; color: black; font-weight: bold">Powiel we wszystkich jezykach</a></li>';
+var statusBox = '<div id="tamperStatusBox" class="msgWrapper" style="width: 90%; max-width: auto; padding: 15px 0 0 0; display: none">' +
+    '<div class="alert alert-block alert-info">' +
+    '<div style="display: table;">' +
+    '<div class="message">' +
+    '<strong>Skopiowane dane:</strong><br/>' +
+    '<strong>Nazwa: </strong><span id="tamperProductcName"></span><br/>' +
+    '<strong>Opis krótki: </strong><span id="tamperProductcShortD"></span><br/>' +
+    '<strong>Opis długi: </strong><span id="tamperProductcLongD"></span><br/>' +
+    '<strong>Zaktualizowano tabów: </strong><span id="tamperNumberOfTabs">0/0</span>' +
+    '</div></div></div></div>';
+var mainTabsId = '#mainTabsIdUlId';
 
-$(button).appendTo('#mainTabsIdUlId');
+
+$(button).appendTo(mainTabsId);
+$(mainTabsId).after(statusBox);
 
 $('#tamperCopyToOther').click(function(){
-   tamperCopyToOther();
+    tamperCopyToOther();
 
    return false;
 });
